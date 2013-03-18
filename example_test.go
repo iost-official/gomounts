4e0777d5ae070b0ff715fb066392cd90310996bd
Clint Caywood <clint@clintcaywood.com>
2013-03-17 20:58:57 -0500

    Add some documentation

---
+++ example_test.go
@@ -0,0 +1,17 @@
+package gomounts_test
+
+import (
+	"github.com/cratonica/gomounts"
+	"log"
+)
+
+func Example() {
+	volumes, err := gomounts.GetMountedVolumes()
+	if err == nil {
+		for _, v := range volumes {
+			// Windows might print "Volume of type NTFS is mounted at C:\"
+			// Unix might print "Volume of type ext3 is mounted at /media/HD1"
+			log.Printf("Volume of type %s is mounted at %s", v.Type, v.Path)
+		}
+	}
+}
